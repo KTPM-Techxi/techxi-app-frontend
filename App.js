79028da415004d1320eb4bcc75323b98e3a3e82c
nhatminhdev <heomeco2014@gmail.com>
2023-06-19 22:54:54 +0700

    init: init techxi-app frontend

---
+++ App.js
@@ -0,0 +1,10 @@
+import { StatusBar } from 'expo-status-bar';
+import { SafeAreaView, Text, View } from 'react-native';
+
+export default function App() {
+    return (
+        <SafeAreaView className="flex h-full justify-center">
+            <Text className="text-red-500 text-center text-3xl">Chăm chỉ cày nhé</Text>
+        </SafeAreaView>
+    );
+}
