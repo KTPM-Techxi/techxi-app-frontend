d09b763a4960aa119107d4d43fd4f6204af6788a
nminhdev <heomeco2014@gmail.com>
2023-08-19 17:57:29 +0700

    feat: finish UI Driver History

--- components/Driver/HistoryComponent.js
+++ components/Driver/HistoryComponent.js
@@ -1,46 +1,80 @@
-import { View, Text, Image } from 'react-native';
+import { View, Text, Image, SafeAreaView } from 'react-native';
 import React from 'react';
+import { Button, IconButton, MD3Colors } from 'react-native-paper';
 
-const TextInfo = ({ primaryText, secondaryText }) => {
+const TextInfo = ({ primaryText, secondaryText, icon = {} }) => {
     return (
-        <View className="flex flex-col ">
-            <Text className="font-bold capitalize text-xl">{primaryText}</Text>
-            <Text className="capitalize mb-1">{secondaryText}</Text>
+        <View className="flex">
+            <View className="flex-row items-center ">{primaryText}</View>
+            <View className="flex-row items-center ">{secondaryText}</View>
         </View>
     );
 };
 
-const HistoryItem = ({ imgSrc, primaryText, secondaryText, style }) => {
+const HistoryItem = ({ imgSrc, primaryText, secondaryText, style, icon, secondaryIcon }) => {
     return (
-        <View className="flex-row items-center bg-yellow-500 rounded-lg p-4 my-1">
+        <SafeAreaView className="flex-row bg-white py-4 px-2">
             <Image
                 source={imgSrc}
-                className={`w-12 h-full rounded-full object-cover ${style} mr-4`}
+                className={`min-w-[60px] h-[60px] rounded-full object-cover ${style}`}
             />
             <TextInfo
-                primaryText={primaryText}
-                secondaryText={secondaryText}
+                primaryText={
+                    <>
+                        {icon}
+                        <Text className="font-bold capitalize text-[16px]">{primaryText}</Text>
+                    </>
+                }
+                secondaryText={
+                    <>
+                        {secondaryIcon}
+                        {secondaryText}
+                    </>
+                }
             />
-        </View>
+        </SafeAreaView>
     );
 };
 
 const HistoryComponent = () => {
     return (
-        <View className="flex bg-blue-300 p-4 rounded-3xl">
-            <HistoryItem
-                imgSrc={{ uri: 'https://legacy.reactjs.org/logo-og.png' }}
-                primaryText="Mrs. Diana Milkzusk"
-                secondaryText="Pickup - 2023 Ford Ecosport"
-            />
-            <HistoryItem
-                imgSrc={{
-                    uri: 'https://images2.thanhnien.vn/Uploaded/trungnq/2022_10_29/1-2829.jpg'
-                }}
-                style="rounded-2xl w-[100px] h-[100px]"
-                primaryText="8.30 AM, Mar 23 2020"
-                secondaryText="1621 Ruelle De Grace, Baton Rouge"
-            />
+        <View className="flex bg-green-300 border-b border-emerald-300">
+            <View className="rounded overflow-hidden">
+                <HistoryItem
+                    imgSrc={{ uri: 'https://legacy.reactjs.org/logo-og.png' }}
+                    primaryText="Mrs. Diana Milkzusk"
+                    secondaryText={
+                        <Text className="text-gray-400 text-[14px] max-w-[260px]">
+                            Pickup - 2023 Ford Ecosport
+                        </Text>
+                    }
+                    style="mr-4 items-center"
+                />
+                <HistoryItem
+                    imgSrc={{
+                        uri: 'https://images2.thanhnien.vn/Uploaded/trungnq/2022_10_29/1-2829.jpg'
+                    }}
+                    style="rounded-2xl w-[100px] h-[100px] rounded mr-2"
+                    primaryText="8.30 AM, Mar 23 2020"
+                    secondaryText={
+                        <Text className="text-gray-400 text-[14px] max-w-[180px] whitespace-nowrap text-ellipsis">
+                            1621 Ruelle De Grace, Baton Rouge
+                        </Text>
+                    }
+                    icon={
+                        <IconButton
+                            icon="clock-time-five"
+                            // color={MD3Colors.error0}
+                        />
+                    }
+                    secondaryIcon={
+                        <IconButton
+                            icon="map-marker"
+                            // color={MD3Colors.error0}
+                        />
+                    }
+                />
+            </View>
         </View>
     );
 };
--- screens/DriverScreens/HistoryScreen.js
+++ screens/DriverScreens/HistoryScreen.js
@@ -2,12 +2,16 @@ import { View, Text } from 'react-native';
 import React from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import HistoryComponent from '../../components/Driver/HistoryComponent';
+import { ScrollView } from 'react-native-gesture-handler';
 
 const HistoryScreen = () => {
     return (
-        <SafeAreaView className=" flex-1 bg-blue-100">
+        <ScrollView className=" flex-1 bg-blue-100">
             <HistoryComponent />
-        </SafeAreaView>
+            <HistoryComponent />
+            <HistoryComponent />
+            <HistoryComponent />
+        </ScrollView>
     );
 };
 
